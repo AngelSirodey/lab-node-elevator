413597c34f2929ea89c1229dd8932b4487813c60
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-11-06 19:22:41 +0100

    First Iteration finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,52 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = 'up';
+    this.waitingList = [];
+    this.passenger = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.interval = setInterval(() => {
+      this.update()
+    }, 500)
+  }
+
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  update() {
+    this.log();
+    if (this.floor < 10 && this.direction == 'up') {
+      this.floorUp()
+    }else if (this.floor == 0){
+      this.floorUp()
+    }else{
+      this.floorDown()
+    }
+  }
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() {
+    this.direction = 'up';
+    this.floor += 1;
+  }
+
+  floorDown() {
+    this.direction = 'down';
+    this.floor -= 1;
+  }
+  call() {
+    this.requests.push()
+  }
+  log() {
+    console.log('direction ' + this.direction + ' ' + 'floor ' + this.floor)
+  }
 }
 
 module.exports = Elevator;
